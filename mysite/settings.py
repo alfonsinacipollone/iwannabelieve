7fc68a12ead4d5a721538e8649ac14e780c2cc56
alfonsinacipollone <alfonsinacipollone@gmail.com>
2017-11-18 17:27:55 -0300

    modifique el host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l8uj$i@oek$%x(^ii2jrue(q_j@@-3_-bm$&m#av*8iz%fa*)&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<tu-usuario>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'truthisoutthere.pythonanywhere.com']
 
 
 # Application definition
